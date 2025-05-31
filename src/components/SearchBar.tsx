650f341a243e90e344845c2ed76a265c2f7ee428
Amir Adel <amiradel1234567@gmail.com>
2025-06-01 01:09:45 +0300

    Handling search feature

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,32 @@
 'use client'
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+
+
 function SearchBar() {
+
+  const router = useRouter();  
+  function handleSearch(e:React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get('name') as string;
+
+    if (name) {
+      router.push(`/search?name=${name}`);
+    }
+  }
+
   return (
     <form 
+      onSubmit={handleSearch}
       role="search"
       className="flex items-center border border-gray-300 rounded-full overflow-hidden focus-within:ring-2 focus-within:ring-blue-500 focus-within:border-transparent"
     >
       <input
         type="text"
+        name = 'name'
         placeholder="Search bicycles..."
         aria-label="Search bicycles"
         className="py-2 px-4 outline-none w-full max-w-[200px] sm:max-w-none"
@@ -14,7 +34,7 @@ function SearchBar() {
       <button 
         type="submit"
         aria-label="Search"
-        className="px-3 py-2 bg-gray-100 hover:bg-gray-200 transition-colors"
+        className="px-3 py-2 cursor-pointer bg-gray-100 hover:bg-gray-200 transition-colors"
       >
         🔍
       </button>
