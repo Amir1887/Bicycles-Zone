f7f08a0bab2414c39f51043b8347f6bdf62212bd
Amir Adel <amiradel1234567@gmail.com>
2025-06-03 20:36:36 +0300

    Initial structure of productList component

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,16 +1,44 @@
-import { Product } from "./types"
+import Image from "next/image";
+import { Product } from "./types";
 interface ProductListProps {
-    productList: Product[]
+  productList: Product[];
 }
 
-function ProductList({ productList }:  ProductListProps ){
+function ProductList({ productList }: ProductListProps) {
   return (
     <div>
-      {productList.map((p) => (
-        <h1>{p.title}</h1>
+      <h1>Featured Products</h1>
+      {productList.map((product) => (
+        // individual product
+        <div>
+          {/* image part  */}
+          <div>
+            <Image
+              src={product.images[0]}
+              alt={product.title}
+              width={40}
+              height={40}
+            />
+            <Image
+              src={product.images[1]}
+              alt={product.title}
+              width={40}
+              height={40}
+            />
+          </div>
+
+          {/* text  */}
+          <div>
+            <h2>{product.title}</h2>
+            <span>{product.price}</span>
+          </div>
+
+          {/* add to cart  */}
+          <button>Add to Cart</button>
+        </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
