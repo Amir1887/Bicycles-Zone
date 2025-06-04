58c5899e57ef690e01b21917480417fe91726b9a
Amir Adel <amiradel1234567@gmail.com>
2025-06-04 20:54:41 +0300

    Footer sectin styling - Basic Grid Structure - Add Contact Section (First Column)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,39 @@
+import Image from "next/image"
+import Link from "next/link"
 
 
 function Footer() {
   return (
-    <footer className="py-24 px-4 md:px-8 lg:px-8 xl:px-16 2xl:px-64">
-      &copy; 2025 Bicycles Zone 
+    <footer className="bg-gray-100 py-16 px-4 md:px-8 lg:px-16 xl:px-32 text-gray-700">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-auto  max-w-7xl">
+          {/* Contact Section (First Column) */}
+          <div className="flex flex-col gap-2">
+            <Link 
+            className="text-lg md:text-2xl lg:text-3xl font-bold"
+             href={'/'}>Bicycle Zone</Link>
+            <address>
+                  Giza, Egypt<br />
+                  <span className="text-orange-600 font-extrabold text-sm">Shipping available across all Egypt</span>
+            </address>
+
+            <div className="flex flex-col gap-2 font-semibold">
+              <a href="mailto:contact@bicyclezone.co">contact@bicyclezone.co</a>
+              <a href="tel:+201067097867">+201067097867</a>
+            </div>
+
+            <div className="flex gap-4 py-2">
+              <Link href={'https://www.facebook.com/profile.php?id=100064058350639'}>
+                <Image src="/facebook.png" alt="Facebook" width={20} height={20} />
+              </Link>
+              <Link href={'https://www.instagram.com/bicycles_zone2'}>
+                <Image src="/instagram.png" alt="Facebook" width={20} height={20} />
+              </Link>
+              <Link href={'tel:+201067097867'}>
+                <Image src="/whatsapp.png" alt="whatsapp" width={20} height={20} />
+              </Link>
+            </div>
+          </div>
+      </div>
     </footer>
   )
 }
